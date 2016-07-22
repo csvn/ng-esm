04df2fc327c213256b86ea50637be5b081f643e1
Christian Svensson <christian.svensson.89@gmail.com>
2016-07-22 16:46:52 +0200

    demo ...

--- example/src/config/state.ts
+++ example/src/config/state.ts
@@ -2,7 +2,7 @@ import { Config, OnInit } from 'ng-esm';
 import { UrlRouterProvider } from 'angular-ui-router';
 
 
-@Config([])
+@Config()
 class StateConfig implements OnInit {
   constructor(private $urlRouterProvider: UrlRouterProvider) {}
 
