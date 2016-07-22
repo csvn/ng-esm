775172406f6a4c22358be11f8b32cd311004c852
Christian Svensson <christian.svensson.89@gmail.com>
2016-07-22 13:30:30 +0200

    State always has uiRouter as dependency

--- src/register/state.ts
+++ src/register/state.ts
@@ -3,19 +3,21 @@ import { config, register } from '../ng';
 import { StateOptions } from '../common';
 import { RESOLVES_SYMBOL } from './resolve';
 
+const UI_ROUTER = 'ui.router';
 
 export function State(options: StateOptions) {
   return function(target: Function | any): void {
     function stateRunner($stateProvider: StateProvider): void {
       options.controller = target;
       options.controllerAs = options.controllerAs || config.ctrlAs;
+      options.dependencies = options.dependencies || [];
+      options.dependencies.push(UI_ROUTER);
 
       if (target[RESOLVES_SYMBOL]) {
         options.resolve = target[RESOLVES_SYMBOL];
       }
 
       $stateProvider.state(options.name, options);
-      console.log(options);
     }
 
     stateRunner.$inject = ['$stateProvider'];
