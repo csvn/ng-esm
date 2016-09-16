ed42b4054d0d346dd65114f7c52cc315b5990c1c
Christian Svensson <christian.svensson.89@gmail.com>
2016-09-16 12:40:53 +0200

    expanded NgModule signature

--- src/register/module.ts
+++ src/register/module.ts
@@ -8,10 +8,26 @@ export interface NgModuleRegistration {
   register(ngModule: ng.IModule): void;
 }
 
-export function NgModule(config: BaseConfig | Dependencies) {
+interface NgModuleSignature {
+  (): Function;
+  (config: BaseConfig): Function;
+  (moduleId: string): Function;
+  (moduleId: string, dependencies?: Dependencies): Function;
+}
+
+export const NgModule: NgModuleSignature = NgModuleDecorator;
+
+
+function NgModuleDecorator(val: string | Dependencies | BaseConfig = {}, deps: Dependencies = []) {
   return function(target: Function | (Function & NgModuleRegistration)): void {
-    if (Array.isArray(config)) {
-      config = { dependencies: config };
+    let config: BaseConfig;
+
+    if (Array.isArray(val)) {
+      config = { dependencies: val };
+    } else if (typeof val === 'string') {
+      config = { name: val, dependencies: deps };
+    } else {
+      config = val;
     }
 
     let ngModule = createModule(target, config.dependencies, name(target, config));
