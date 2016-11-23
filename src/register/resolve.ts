2d14b9745bd2dd3164ef3bc63a5f366d354223b3
Christian Svensson <christian.svensson.89@gmail.com>
2016-11-23 12:37:35 +0100

    @Resolve interface to match ui-router

--- src/register/resolve.ts
+++ src/register/resolve.ts
@@ -1,30 +1,31 @@
 import * as ng from 'angular';
+import { IInjectable } from 'angular-ui-router';
 
 declare const angular: ng.IAngularStatic;
 
 
 export const RESOLVES_SYMBOL = Symbol('resolves');
+export type ResolveTarget = Function;
+
+namespace ResolveTarget {}
 
 /** Decorate static class method as a resolve property (prop name => resolve name) */
 export function resolve(
-  constructor: any | Resolver, name: string, descriptor: PropertyDescriptor
+  constructor: Function, name: string, descriptor: PropertyDescriptor
 ) {
   if (!angular.isFunction(descriptor.value)) {
     throw new Error('@resolve can only be used on static class methods');
   }
-  constructor[RESOLVES_SYMBOL] = constructor[RESOLVES_SYMBOL] || {};
-  constructor[RESOLVES_SYMBOL][name] = descriptor.value;
-}
-
-/** Injectable function that will resolve to a value or a Promise */
-export interface Resolver {
-  (...injectables: any[]): any | Promise<any>;
+  const target: any = constructor;
+  target[RESOLVES_SYMBOL] = target[RESOLVES_SYMBOL] || {};
+  target[RESOLVES_SYMBOL][name] = descriptor.value;
 }
 
 /** Decorate a class (that is decorated with `@State()`) with it's resolve values */
-export function Resolve(resolves: { [name: string]: Resolver }) {
+export function Resolve(resolves: { [name: string]: IInjectable }) {
   // target requires type "any" due to symbol properties not being supported
-  return function(target: Function | any): void {
+  return function(constructor: Function): void {
+    const target: any = constructor;
     target[RESOLVES_SYMBOL] = resolves;
   };
 }
